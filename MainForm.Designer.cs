37c00ab93e47ff3d12a8772ef6780a4e6c99a374
Akin Sibay <sibayakin@gmail.com>
2020-10-07 19:29:57 +0300

    arduino connection gui

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -45,6 +45,11 @@
             this.ExtraStepIlaveTxt = new System.Windows.Forms.TextBox();
             this.ExtraStepIlaveLbl = new System.Windows.Forms.Label();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.TersYonCheck = new System.Windows.Forms.CheckBox();
+            this.baglanBtn = new System.Windows.Forms.Button();
+            this.baglantiKesBtn = new System.Windows.Forms.Button();
+            this.baglantiStatus = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -96,7 +101,7 @@
             // 
             this.HizlanBtn.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.HizlanBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.HizlanBtn.Location = new System.Drawing.Point(279, 134);
+            this.HizlanBtn.Location = new System.Drawing.Point(285, 229);
             this.HizlanBtn.Name = "HizlanBtn";
             this.HizlanBtn.Size = new System.Drawing.Size(148, 33);
             this.HizlanBtn.TabIndex = 8;
@@ -108,7 +113,7 @@
             // 
             this.StepLbl.AutoSize = true;
             this.StepLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.StepLbl.Location = new System.Drawing.Point(235, 5);
+            this.StepLbl.Location = new System.Drawing.Point(240, 5);
             this.StepLbl.Name = "StepLbl";
             this.StepLbl.Size = new System.Drawing.Size(20, 24);
             this.StepLbl.TabIndex = 5;
@@ -120,7 +125,7 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.StepLbl);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 184);
+            this.panel1.Location = new System.Drawing.Point(0, 279);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(464, 38);
             this.panel1.TabIndex = 13;
@@ -129,17 +134,17 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label4.Location = new System.Drawing.Point(152, 5);
+            this.label4.Location = new System.Drawing.Point(63, 5);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(58, 24);
+            this.label4.Size = new System.Drawing.Size(153, 24);
             this.label4.TabIndex = 6;
-            this.label4.Text = "Step :";
+            this.label4.Text = "Current Position :";
             // 
             // DurdurBtn
             // 
             this.DurdurBtn.BackColor = System.Drawing.Color.Tomato;
             this.DurdurBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.DurdurBtn.Location = new System.Drawing.Point(24, 134);
+            this.DurdurBtn.Location = new System.Drawing.Point(39, 229);
             this.DurdurBtn.Name = "DurdurBtn";
             this.DurdurBtn.Size = new System.Drawing.Size(148, 33);
             this.DurdurBtn.TabIndex = 14;
@@ -154,9 +159,9 @@
             this.groupBox1.Controls.Add(this.HizTxt);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.groupBox1.Location = new System.Drawing.Point(24, 12);
+            this.groupBox1.Location = new System.Drawing.Point(30, 88);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(192, 113);
+            this.groupBox1.Size = new System.Drawing.Size(192, 108);
             this.groupBox1.TabIndex = 15;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Ana Program";
@@ -167,9 +172,9 @@
             this.groupBox2.Controls.Add(this.ExtraStepIlaveTxt);
             this.groupBox2.Controls.Add(this.ExtraStepIlaveLbl);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.groupBox2.Location = new System.Drawing.Point(235, 12);
+            this.groupBox2.Location = new System.Drawing.Point(241, 88);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(192, 113);
+            this.groupBox2.Size = new System.Drawing.Size(192, 108);
             this.groupBox2.TabIndex = 16;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "İlave Program";
@@ -208,14 +213,75 @@
             // timer
             // 
             this.timer.Enabled = true;
-            this.timer.Interval = 2000;
+            this.timer.Interval = 1000;
             this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // TersYonCheck
+            // 
+            this.TersYonCheck.AutoSize = true;
+            this.TersYonCheck.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TersYonCheck.Location = new System.Drawing.Point(346, 202);
+            this.TersYonCheck.Name = "TersYonCheck";
+            this.TersYonCheck.Size = new System.Drawing.Size(85, 21);
+            this.TersYonCheck.TabIndex = 14;
+            this.TersYonCheck.Text = "Ters Yön";
+            this.TersYonCheck.UseVisualStyleBackColor = true;
+            // 
+            // baglanBtn
+            // 
+            this.baglanBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baglanBtn.Location = new System.Drawing.Point(30, 39);
+            this.baglanBtn.Name = "baglanBtn";
+            this.baglanBtn.Size = new System.Drawing.Size(87, 29);
+            this.baglanBtn.TabIndex = 17;
+            this.baglanBtn.Text = "Bağlan";
+            this.baglanBtn.UseVisualStyleBackColor = true;
+            this.baglanBtn.Click += new System.EventHandler(this.baglanBtn_Click);
+            // 
+            // baglantiKesBtn
+            // 
+            this.baglantiKesBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baglantiKesBtn.Location = new System.Drawing.Point(123, 39);
+            this.baglantiKesBtn.Name = "baglantiKesBtn";
+            this.baglantiKesBtn.Size = new System.Drawing.Size(106, 29);
+            this.baglantiKesBtn.TabIndex = 18;
+            this.baglantiKesBtn.Text = "Bağlantıyı Kes";
+            this.baglantiKesBtn.UseVisualStyleBackColor = true;
+            this.baglantiKesBtn.Click += new System.EventHandler(this.baglantiKesBtn_Click);
+            // 
+            // baglantiStatus
+            // 
+            this.baglantiStatus.Dock = System.Windows.Forms.DockStyle.Top;
+            this.baglantiStatus.FlatAppearance.BorderSize = 0;
+            this.baglantiStatus.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.baglantiStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.baglantiStatus.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.baglantiStatus.Location = new System.Drawing.Point(0, 0);
+            this.baglantiStatus.Name = "baglantiStatus";
+            this.baglantiStatus.Size = new System.Drawing.Size(464, 24);
+            this.baglantiStatus.TabIndex = 20;
+            this.baglantiStatus.Text = "BAĞLANDI";
+            this.baglantiStatus.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "label3";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(464, 222);
+            this.ClientSize = new System.Drawing.Size(464, 317);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.baglantiStatus);
+            this.Controls.Add(this.baglantiKesBtn);
+            this.Controls.Add(this.baglanBtn);
+            this.Controls.Add(this.TersYonCheck);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.HizlanBtn);
@@ -231,6 +297,7 @@
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -252,5 +319,10 @@
         private System.Windows.Forms.Label ExtraStepIlaveLbl;
         private System.Windows.Forms.CheckBox IlaveCheckBox;
         private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox TersYonCheck;
+        private System.Windows.Forms.Button baglanBtn;
+        private System.Windows.Forms.Button baglantiKesBtn;
+        private System.Windows.Forms.Button baglantiStatus;
+        private System.Windows.Forms.Label label3;
     }
 }
--- MainForm.cs
+++ MainForm.cs
@@ -14,6 +14,7 @@ namespace AccelStepperSample
     public partial class MainForm : Form
     {
         public static double maxSpeed = 3500.0;
+        public static string hiz, extraStepAna, extraStepIlave;
         public MainForm()
         {
             InitializeComponent();
@@ -25,51 +26,61 @@ namespace AccelStepperSample
             ExtraStepIlaveTxt.Text = "0";
             ExtraStepIlaveLbl.Visible = false;
             ExtraStepIlaveTxt.Visible = false;
-            try
-            {
-                arduino.Open();
-                Text = Text + " (BAĞLANDI)";
-            }
-            catch (Exception)
-            {
-                MessageBox.Show("COM bağlantısı kapalı. Lütfen tekrar deneyin.");
-                Application.Exit();
-            }
+            
+            arduinoConnect();
         }
         
         private void HizlanBtn_Click(object sender, EventArgs e)
         {
-            if (IlaveCheckBox.Checked == true)
+            YonCheck();
+            if (arduino.IsOpen)
             {
-                IlaveProgramBaslat();
+                if (IlaveCheckBox.Checked == true)
+                {
+                    IlaveProgramBaslat();
+                }
+                else
+                {
+                    AnaProgramBaslat();
+                }
             }
             else
             {
-                AnaProgramBaslat();
+                MessageBox.Show("Port bağlantısı kapalı. Açıp tekrar deneyin.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
        
         private void AnaProgramBaslat()
         {
-            string command = TextHazirla("B", HizTxt.Text, ExtraStepAnaTxt.Text, ExtraStepIlaveTxt.Text);
+            string command = TextHazirla("B", hiz, extraStepAna, extraStepIlave);
+            //MessageBox.Show(command);
             arduino.Write(command);
             Thread.Sleep(100);
-            //StepLbl.Text = arduino.ReadLine();
+            StepLbl.Text = arduino.ReadLine();
             ExtraStepAnaTxt.Text = "0";
         }
         private void IlaveProgramBaslat()
         {
-            string command = TextHazirla("C", HizTxt.Text, ExtraStepAnaTxt.Text, ExtraStepIlaveTxt.Text);
+            string command = TextHazirla("C", hiz, extraStepAna, extraStepIlave);
+            //MessageBox.Show(command);
             arduino.Write(command);
             Thread.Sleep(100);
-            //StepLbl.Text = arduino.ReadLine();
+            StepLbl.Text = arduino.ReadLine();
             ExtraStepAnaTxt.Text = "0";
         }
 
         private void DurdurBtn_Click(object sender, EventArgs e)
         {
-            string command = TextHazirla("D", HizTxt.Text, ExtraStepAnaTxt.Text, ExtraStepIlaveTxt.Text);
-            arduino.Write(command);
+            if (arduino.IsOpen)
+            {
+                string command = TextHazirla("D", HizTxt.Text, ExtraStepAnaTxt.Text, ExtraStepIlaveTxt.Text);
+                arduino.Write(command);
+            }
+            else
+            {
+                MessageBox.Show("Port bağlantısı kapalı. Açıp tekrar deneyin.","", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
         }
 
         private static string TextHazirla(string command, string t1, string t2, string t3)
@@ -79,7 +90,10 @@ namespace AccelStepperSample
 
         private void timer_Tick(object sender, EventArgs e)
         {
-            //PERİYODİK;
+            if (arduino.IsOpen)
+            {
+                label3.Text = arduino.ReadExisting();
+            }      
         }
 
         private void IlaveCheckBox_CheckedChanged(object sender, EventArgs e)
@@ -98,33 +112,136 @@ namespace AccelStepperSample
 
         private void HizTxt_TextChanged(object sender, EventArgs e)
         {
-            if (System.Text.RegularExpressions.Regex.IsMatch(HizTxt.Text, "[^0-9]"))
+            try
             {
-                MessageBox.Show("Sadece tam sayı girilebilir.");
-                HizTxt.Text = HizTxt.Text.Remove(HizTxt.Text.Length - 1);
+                if (System.Text.RegularExpressions.Regex.IsMatch(HizTxt.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Sadece tam sayı girilebilir.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    HizTxt.Text = HizTxt.Text.Remove(HizTxt.Text.Length - 1);
+                }
+                if (Convert.ToInt32(HizTxt.Text) > maxSpeed)
+                {
+                    MessageBox.Show("Hız en fazla " + maxSpeed + " olabilir.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    HizTxt.Text = HizTxt.Text.Remove(HizTxt.Text.Length - 1);
+                }
             }
-            if (Convert.ToInt32(HizTxt.Text) > maxSpeed)
+            catch (FormatException)
             {
-                MessageBox.Show("Hız en fazla "+maxSpeed+" olabilir.");
-                HizTxt.Text = HizTxt.Text.Remove(HizTxt.Text.Length - 1);
+                HizTxt.Text = "0";
             }
+            
         }
 
         private void ExtraStepAnaTxt_TextChanged(object sender, EventArgs e)
         {
-            if (System.Text.RegularExpressions.Regex.IsMatch(ExtraStepAnaTxt.Text, "[^0-9]"))
+            try
             {
-                MessageBox.Show("Sadece tam sayı girilebilir.");
-                ExtraStepAnaTxt.Text = ExtraStepAnaTxt.Text.Remove(ExtraStepAnaTxt.Text.Length - 1);
+                if (System.Text.RegularExpressions.Regex.IsMatch(ExtraStepAnaTxt.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Sadece tam sayı girilebilir.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ExtraStepAnaTxt.Text = ExtraStepAnaTxt.Text.Remove(ExtraStepAnaTxt.Text.Length - 1);
+                }
+                if(ExtraStepAnaTxt.Text == "")
+                {
+                    ExtraStepAnaTxt.Text = "0";
+                }
             }
+            catch (FormatException)
+            {
+                ExtraStepAnaTxt.Text = "0";
+            }
+            
         }
 
         private void ExtraStepIlaveTxt_TextChanged(object sender, EventArgs e)
         {
-            if (System.Text.RegularExpressions.Regex.IsMatch(ExtraStepIlaveTxt.Text, "[^0-9]"))
+            try
             {
-                MessageBox.Show("Sadece tam sayı girilebilir.");
-                ExtraStepIlaveTxt.Text = ExtraStepIlaveTxt.Text.Remove(ExtraStepIlaveTxt.Text.Length - 1);
+                if (System.Text.RegularExpressions.Regex.IsMatch(ExtraStepIlaveTxt.Text, "[^0-9]"))
+                {
+                    MessageBox.Show("Sadece tam sayı girilebilir.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    ExtraStepIlaveTxt.Text = ExtraStepIlaveTxt.Text.Remove(ExtraStepIlaveTxt.Text.Length - 1);
+                }
+                if (ExtraStepIlaveTxt.Text == "")
+                {
+                    ExtraStepIlaveTxt.Text = "0";
+                }
+            }
+            catch (FormatException)
+            {
+                ExtraStepIlaveTxt.Text = "0";
+            }
+            
+        }
+
+        private void YonCheck()
+        {
+            if (TersYonCheck.Checked)
+            {
+                hiz = HizTxt.Text;
+                extraStepAna = (Convert.ToInt32(ExtraStepAnaTxt.Text) * -1).ToString();
+                extraStepIlave = (Convert.ToInt32(ExtraStepIlaveTxt.Text) * -1).ToString();
+            }
+            else
+            {
+                hiz = HizTxt.Text;
+                extraStepAna = ExtraStepAnaTxt.Text;
+                extraStepIlave = ExtraStepIlaveTxt.Text;
+            }
+        }
+
+        private void baglanBtn_Click(object sender, EventArgs e)
+        {
+            if (!arduino.IsOpen)
+            {
+                arduinoConnect();
+            }
+            else
+            {
+                MessageBox.Show("Zaten bağlı.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void baglantiKesBtn_Click(object sender, EventArgs e)
+        {
+            if (arduino.IsOpen)
+            {
+                arduinoDisconnect();
+            }
+            else
+            {
+                MessageBox.Show("Zaten bağlantı kapalı.", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void arduinoConnect()
+        {
+            try
+            {
+                arduino.Open();
+                Text = "Pompa Uygulaması (BAĞLANDI)";
+                baglantiStatus.BackColor = Color.Green;
+                baglantiStatus.Text = "BAĞLANDI";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("COM bağlantısı kapalı. Lütfen tekrar deneyin.","", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            
+        }
+        private void arduinoDisconnect()
+        {
+            try
+            {
+                arduino.Close();
+                Text = "Pompa Uygulaması (BAĞLANTI KESİLDİ)";
+                baglantiStatus.BackColor = Color.Red;
+                baglantiStatus.Text = "BAĞLANTI KESİLDİ";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
             }
         }
     }
